226495d8a26642b2b707de038b9ff34bcba41447
ermi14 <ermikasa@gmail.com>
2020-11-19 09:31:53 +0300

    details page error solved

--- lib/screens/home/components/popular_product.dart
+++ lib/screens/home/components/popular_product.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_native_interaction/models/Product.dart';
-// import 'package:shop_app/components/product_card.dart';
-// import 'package:shop_app/models/Product.dart';
+import 'package:flutter_native_interaction/components/product_card.dart';
+import 'package:flutter_native_interaction/models/Product.dart';
 
 import '../../../size_config.dart';
 import 'section_title.dart';
@@ -25,7 +25,7 @@ class PopularProducts extends StatelessWidget {
                 demoProducts.length,
                 (index) {
                   if (demoProducts[index].isPopular)
-                   // return ProductCard(product: demoProducts[index]);
+                   return ProductCard(product: demoProducts[index]);
 
                   return SizedBox
                       .shrink(); // here by default width and height is 0
